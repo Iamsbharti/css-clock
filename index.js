91826cc8a7d9fb5755a05f37d2b87a3969ea657e
Saurabh <saurabhbharti9@gmail.com>
2020-05-15 14:15:16 +0530

    added hours manipulations and added styles and fixed transition problem

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ function setDate() {
   //minute hand manipulation
   let minutesHand = document.querySelector(".min-hand");
   const minute = now.getMinutes();
-  console.log(minute);
+  //console.log(minute);
   const minutesDegree = (minute / 60) * 360 + 90;
   minutesHand.style.transform = `rotate(${minutesDegree}deg)`;
 
   //hours hand manipulation
+  let hourHand = document.querySelector(".hour-hand");
+  const hour = now.getHours();
+  const hourDegree = (hour / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hourDegree}deg)`;
 }
 setInterval(setDate, 1000);
